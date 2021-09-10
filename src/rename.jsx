e24170eb94be5738f5583da1b6121de1737f4b5d
DwnNyxDev <vatelvandell@gmail.com>
2021-09-10 00:03:28 -0400

    Patched program to read layers in groups + program ignores users who have no cards added

--- src/rename.jsx
+++ src/rename.jsx
@@ -4,39 +4,47 @@
 var docs = app.documents;
 
 for(var user in users){
-    var user_folder = Folder(save_location+"/"+users[user].names[0]);
-    if(!user_folder.exists){
-        user_folder.create();
-    }
-    
-    for(var u=0; u<users[user].cards.length; u++){
-        var card = users[user].cards[u];
-        var cLimit=20;
-        var replaceString="Name";
-        for(var psdFind in psds){
-            if(psds[psdFind].name==card){
-                cLimit=psds[psdFind].limit;
-                replaceString=psds[psdFind].replace;
-                break;
-            }
+    if(users[user].cards.length>0){
+        var user_folder = Folder(save_location+"/"+users[user].names[0]);
+        if(!user_folder.exists){
+            user_folder.create();
         }
-        var user_name = users[user].names[0];
-        if(user_name.length>cLimit&&users[user].names.length>1){
-            user_name=users[user].names[1];
-        }
-        var card_file = new File(user_folder+"/"+user_name+"_"+card.replace(".psd","")+".png");
-        if(!card_file.exists){
-            app.activeDocument = documents.getByName(card);
-            app.activeDocument = app.activeDocument.duplicate();
-            var layers = app.activeDocument.layers;
-            for (var l=0; l<layers.length;l++){
-                if(layers[l].kind == LayerKind.TEXT){
-                    layers[l].textItem.contents=layers[l].textItem.contents.replace(replaceString,user_name);
+        
+        for(var u=0; u<users[user].cards.length; u++){
+            var card = users[user].cards[u];
+            var cLimit=20;
+            var replaceString="Name";
+            for(var psdFind in psds){
+                if(psds[psdFind].name==card){
+                    cLimit=psds[psdFind].limit;
+                    replaceString=psds[psdFind].replace;
+                    break;
                 }
             }
-            var pngOpts = new PNGSaveOptions;
-            app.activeDocument.saveAs(card_file,pngOpts,true,Extension.LOWERCASE);
-            app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+            var user_name = users[user].names[0];
+            if(user_name.length>cLimit&&users[user].names.length>1){
+                user_name=users[user].names[1];
+            }
+            var card_file = new File(user_folder+"/"+user_name+"_"+card.replace(".psd","")+".png");
+            if(!card_file.exists){
+                app.activeDocument = documents.getByName(card);
+                app.activeDocument = app.activeDocument.duplicate();
+                renameLayers(app.activeDocument);
+                function renameLayers(l){
+                    for(var i=0; i<l.layers.length;i++){
+                        var layer= l.layers[i];
+                        if(layer.layers!=null){
+                            renameLayers(layer);
+                        }
+                        if(layer.kind == LayerKind.TEXT){
+                            layer.textItem.contents=layer.textItem.contents.replace(replaceString,user_name);
+                        }
+                    }
+                }
+                var pngOpts = new PNGSaveOptions;
+                app.activeDocument.saveAs(card_file,pngOpts,true,Extension.LOWERCASE);
+                app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+            }
         }
     }
 }
